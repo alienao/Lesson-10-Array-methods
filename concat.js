7f885883a9b723ed8af6553b4da206ded17216f3
alienao <armin.orujyan@gmail.com>
2021-05-13 00:37:07 +0400

    build: concat.js

---
+++ concat.js
@@ -0,0 +1,7 @@
+//Concatenate the two arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = [5, 6, 7, 8, 9];
+//const arr3; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
